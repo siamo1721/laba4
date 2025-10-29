a46b02ae771aaeb623422ba2dacc6102128ea324
siamo1721 <siamocsgo@gmail.com>
2025-10-29 16:50:12 +0300

    feat(app): Добавление POST-запроса

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,9 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post()
+  echo(@Body() data: any){
+    return this.appService.echo(data);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,11 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  echo(data: any): any{
+    return {
+      status: 'received',
+      you_sent: data,
+      length: data ? JSON.stringify(data) : null,
+    }
+  }
 }
